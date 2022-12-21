d47a5c5ded8f023bec9f9312337634db9ac90ede
Daksh <daksh21036@iiitd.ac.in>
2022-12-21 19:35:55 +0530

    today's 2nd commit

--- Tank Stars/core/src/com/mygdx/game/Play_Screen.java
+++ Tank Stars/core/src/com/mygdx/game/Play_Screen.java
@@ -21,7 +21,7 @@ public class Play_Screen extends ScreenAdapter {
 
     public Play_Screen(TankStars game) {
         this.newGame = game;
-        playscreen = new Sprite(new Texture(Gdx.files.internal("bg.jpeg")));
+        playscreen = new Sprite(new Texture(Gdx.files.internal("bg.png")));
         playscreen.setPosition(0, 0);
         playscreen.setSize(844, 475);
 
@@ -33,7 +33,7 @@ public class Play_Screen extends ScreenAdapter {
         renderer = new OrthogonalTiledMapRenderer(map);
 
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
+        camera.setToOrtho(false, 844, 475);
         camera.update();
     }
 
